7ae9be6944d28d6440a2bce6301a5c7e5ca296b9
Efte Hassan <eftehassanpp@gmail.com>
2025-10-25 08:10:31 +0100

    solved setProxy issue

--- test/client_test.go
+++ test/client_test.go
@@ -7,15 +7,22 @@ import (
 )
 
 func Test(t *testing.T) {
-	client, err := super_client.NewSuperClient(&super_client.ClientConfig{})
+	client, err := super_client.NewSuperClient(&super_client.ClientConfig{BaseProxy: "http://SofagJ-cc-ES-pool-p2p-sessionid-change:ZHlQm1@proxy.zettaproxies.io:8888"})
 	if err != nil {
 		return
 	}
+	// client.SetProxy("http://SofagJ-cc-ES-pool-p2p-sessionid-WpKgt6P6barF:ZHlQm1@proxy.zettaproxies.io:8888")
+	t.Logf("Current Proxy: %s", client.GetProxy())
+	err = client.SetNewProxy()
+	if err != nil {
+		t.Log(err)
+	}
 	var output struct {
 		IP   string `json:"ip"`
-		City string `jons:"city"`
+		City string `json:"city"`
 	}
 	response := client.Get("https://ipinfo.io/json", nil, nil, &output)
 	t.Log(response.StatusCode)
 	t.Log(output)
+	t.Logf("Current Proxy: %s", client.GetProxy())
 }
